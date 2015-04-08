65cbb20378804e1064da8ea4391eab9fd7e26a26
Nicolas Servant <servant.nicolas@gmail.com>
2015-04-08 14:59:02 +0200

    des tests

--- index.php
+++ index.php
@@ -1,67 +1,79 @@
 <?php
 
-session_start();
-
-require "vendor/autoload.php";
-
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
+require("vendor/autoload.php");
 
 const APPID = "662050003940490";
 const APPSECRET = "157067a54d1cf720aa4a524b04f55eb3";
-
 FacebookSession::setDefaultApplication(APPID, APPSECRET);
-
 $helper = new FacebookRedirectLoginHelper('https://nseb1zer.herokuapp.com/');
-$loginUrl = $helper->getLoginUrl();
-$session = $helper->getSessionFromRedirect();
-
-?>
-<!DOCTYPE html>
-<html>
-<head lang="fr">
-    <meta charset="UTF-8">
-    <title></title>
-    <script>
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: '<?php echo APPID; ?>',
-                xfbml: true,
-                version: 'v2.3'
-            });
-        };
-
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {
-                return;
-            }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "//connect.facebook.net/fr_FR/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-</head>
-<body>
-
-<div class="fb-like"
-     data-share="true"
-     data-width="450"
-     data-show-faces="true">
-</div>
-
-<hr>
-
-<?php
-if (isset($session) && isset($_SESSION['fb_token'])) {
+if (isset($_SESSION) && isset($_SESSION['fb_token'])) {
     $session = new FacebookSession($_SESSION['fb_token']);
 } else {
-    var_dump($session);
-    var_dump($_SESSION['fb_token']);
-    ?>
-    <a href="<?php echo $loginUrl; ?>">Se connecter</a>
-    <?php
+    $session = $helper->getSessionFromRedirect();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Titre de ma page</title>
+    <meta name="description" content="description de ma page">
+
+</head>
+<body>
+<script>
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '<?php echo APPID;?>',
+            xfbml: true,
+            version: 'v2.3'
+        });
+    };
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+
+<h1>Mon application facebook</h1>
+
+<div
+    class="fb-like"
+    data-share="true"
+    data-width="450"
+    data-show-faces="true">
+</div>
+<br>
+
+<div class="fb-comments" data-href="https://nseb1zer.herokuapp.com/" data-numposts="5" data-colorscheme="light"></div>
+<br>
+
+<?php
+if ($session) {
+
+    $token = (string)$session->getAccessToken();
+    $_SESSION['fb_token'] = $token;
+    //Prepare
+    $request = new FacebookRequest($session, 'GET', '/me');
+    //execute
+    $response = $request->execute();
+    //transform la data graphObject
+    $user = $response->getGraphObject("Facebook\GraphUser");
+    echo "<pre>";
+    print_r($user);
+    echo "</pre>";
+
+} else {
+    $loginUrl = $helper->getLoginUrl();
+    echo "<a href='" . $loginUrl . "'>Se connecter</a>";
 }
 ?>
 
